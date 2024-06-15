ed926d7ee8bf2041a52a50a71df8e5275c77759b
wangelca <angelicaverz31@gmail.com>
2024-06-14 22:20:06 -0600

    //Lab 2 Changes Made

---
+++ app/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ToDoList />
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+export default App;
